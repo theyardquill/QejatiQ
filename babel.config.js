7e33660c474ec233208ae8ff848b33c1d21e8f38
theyardquill <lmatwere@gmail.com>
2025-12-15 23:25:05 +0300

    assets rebase

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,24 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+    ],
+    plugins: [
       "nativewind/babel",
+
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./",
+          },
+        },
+      ],
+
+      // ⚠️ MUST BE LAST
+      "react-native-reanimated/plugin",
     ],
   };
 };
